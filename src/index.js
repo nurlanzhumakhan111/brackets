0fb19847957b505ee51676fbe88cace5bbf29f83
unknown <nurlanzumahan@gmaul.com>
2020-10-19 20:02:07 +0600

    feat: add solution to brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    while(str !== ''){
+      let counter = 0;
+      for (let i of bracketsConfig) {
+          let brackets = i[0] + i[1];
+          if (str.includes(brackets)){
+              str = str.replace(brackets, '');
+              counter += 1;
+          }
+      }
+      if (counter === 0) break;
+    }
+    if (str === '') return true;
+    return false;
 }
